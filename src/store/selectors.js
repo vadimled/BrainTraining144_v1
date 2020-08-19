da169c9a22f75fd745b5b2cc6bbb36edd207eb8b
@vadim17 <vadim@genesort.com>
2020-08-19 20:09:39 +0300

    rendering array with another length

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,12 +1,13 @@
 import { createSelector } from 'reselect';
 import { config } from '../utils/constants';
 
-export const
-  getListItems = (state) => state.game.full144List,
+export const getListItems = (state) => state.game.full144List,
   getCurrentGameType = (state) => state.game.currentGameType,
-  getFiguresByCurrentType = createSelector(
-    getListItems, getCurrentGameType,
-    (arr, type) => {
-    arr.length = config.type[type]?.gameField;
-    return arr;
+  getFiguresByCurrentType = createSelector(getListItems, getCurrentGameType, (arr, type) => {
+    const length = config.type[type]?.gameField;
+    let newArr = [];
+    for (let i = 0; i < length; i++) {
+      newArr.push(arr[i]);
+    }
+    return newArr;
   });
